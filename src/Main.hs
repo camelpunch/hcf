46a9f686042b99ee3d3cfe5798447edc2f2aa3c1
Andrew Bruce <me@andrewbruce.net>
2015-11-14 20:46:38 +0000

    Fix indent

--- src/Main.hs
+++ src/Main.hs
@@ -4,5 +4,5 @@ import Network.Wreq
 import Control.Lens
 
 main = do
-     r <- get "https://api.run.pivotal.io/v2/info"
-     print (view responseBody r)
+    r <- get "https://api.run.pivotal.io/v2/info"
+    print (view responseBody r)
