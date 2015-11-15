c5d93216ea24cd4211cc208642a9ea047fa91cf6
Andrew Bruce <me@andrewbruce.net>
2015-11-15 16:30:15 +0000

    Print a Haskell type instead of raw JSON

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Network.Wreq
 import Control.Lens
+import Data.Aeson (Value)
+import Data.Map as Map
+import Network.Wreq
+
+type Resp = Response (Map String Value)
 
 main = do
-    r <- get "https://api.run.pivotal.io/v2/info"
+    let opts = defaults & header "Accept" .~ ["application/json"]
+    r <- asJSON =<< getWith opts "https://uaa.run.pivotal.io/info" :: IO Resp
     print (view responseBody r)
